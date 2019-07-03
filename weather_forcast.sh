877c8375e1de841d24093e8732cd3ae184df8069
mckeevew <mckeevew@gmail.com>
2019-07-03 17:37:46 +0000

    Modify Comments.

--- weather_forcast.sh
+++ weather_forcast.sh
@@ -3,10 +3,12 @@
 ${DIALOG_OK=0}
 ${DIALOG_CANCEL=1}
 
+#Display message box
 dialog_box(){
 	dialog --clear --msgbox "$1" 0 0 2>&1 1>&3
 }
 
+#Make List
 list_maker(){
 	output=""
 	for i in $1; do
@@ -16,10 +18,6 @@ list_maker(){
 }
 
 #User info:
-#Current user/all currently logged users - the 'users' command will be useful here
-#User's primary group - the command 'id -gn (username)' (the n option will print the name)
-#User's list of groups (names & numbers) - 'id -G' prints numbers, 'id -Gn' prints names
-#Rework to ensure outputs are human readable
 user_menu(){
 	while true; do
 		choice=$(dialog \
@@ -46,16 +44,8 @@ user_menu(){
 		esac
 	done
 }
+
 #System Menu:
-#Number of processors
-#Amount of memory - the 'free -h' command should be useful here
-#Amount of disk space - the 'df -h' command should be useful here
-#How much space the user's home directory is taking up -the 'du -h' command should be useful here
-#Hostname - the 'hostname' expansion/command should be useful here
-#ip address - the 'ip' or 'hostname' commands should be useful here
-#Update 6/30/19
-#Rework to ensure outputs are human readable
-#I believe the third number in the "--menu" line indicates the number of menu options.
 system_menu(){
 	while true; do
 		choice=$(dialog \
@@ -72,12 +62,10 @@ system_menu(){
 		case $return_value in
 			$DIALOG_OK)
 				case $choice in
-					#not quite sure how to find processor number yet
 					1)
 						dialog_box "The are $(grep -c ^processor /proc/cpuinfo) processors";;
 					2)
 						dialog_box "$(free -h|grep Mem|tr -s ' '|cut -f3 -d ' ') have been used, $(free -h|grep Mem|tr -s ' '|cut -f4 -d ' ') are still free";;
-					#I have no idea how accuarate the output of this command is.
 					3)
 						dialog_box "$(df -h --total|grep total|tr -s ' '|cut -f3 -d ' ') have been used, $(df -h --total|grep total|tr -s ' '|cut -f4 -d ' ') are still free";;
 					4)
@@ -92,6 +80,7 @@ system_menu(){
 	done
 }
 
+#Local Menu:
 local_menu(){
 	while true; do
 		choice=$(dialog \
@@ -106,9 +95,9 @@ local_menu(){
 			$DIALOG_OK)
 				case $choice in
 					1)
-						find_location;;
+						find_location;;			#Go to find location
 					2)
-						find_weather;;
+						find_weather;;			#Go to find weather
 				esac;;
 			$DIALOG_CANCEL)
 				return;;
@@ -116,32 +105,35 @@ local_menu(){
 	done
 }
 
+#Find Location:
 find_location(){
-	echo 0 | dialog --title "Getting IP Address." --gauge "Please wait ...." 10 60
-	myIP=$(curl -s ipinfo.io/ip)
-	echo 33 | dialog --title "Getting Latitude." --gauge "Please wait ...." 10 60
-	lat=$(curl -s https://ipvigilante.com/$myIP|jq '.data.latitude'|tr -d '"')
-	echo 66 | dialog --title "Getting Longitude." --gauge "Please wait ...." 10 60
-	long=$(curl -s https://ipvigilante.com/$myIP|jq '.data.longitude'|tr -d '"')
-	echo 100 | dialog --title "Finalizing." --gauge "Done ...." 10 60
-	dialog_box "Current latitude is ${lat} and longitude is ${long}"
+	echo 0 | dialog --title "Getting IP Address." --gauge "Please wait ...." 10 60		#Display Loading Bar
+	myIP=$(curl -s ipinfo.io/ip)														#Get current IP address
+	echo 33 | dialog --title "Getting Latitude." --gauge "Please wait ...." 10 60		#Display Loading Bar
+	lat=$(curl -s https://ipvigilante.com/$myIP|jq '.data.latitude'|tr -d '"')			#Get my latitude
+	echo 66 | dialog --title "Getting Longitude." --gauge "Please wait ...." 10 60		#Display Loading Bar
+	long=$(curl -s https://ipvigilante.com/$myIP|jq '.data.longitude'|tr -d '"')		#Get my longitude
+	echo 100 | dialog --title "Finalizing." --gauge "Done ...." 10 60					#Display Loading Bar
+	dialog_box "Current latitude is ${lat} and longitude is ${long}"					#Display latitude and longitude
 }
 
+#Find Weather:
 find_weather(){
-	echo 0 | dialog --title "Getting IP Address." --gauge "Please wait ...." 10 60
-	myIP=$(curl -s ipinfo.io/ip)
-	echo 20 | dialog --title "Getting Latitude." --gauge "Please wait ...." 10 60
-	lat=$(curl -s https://ipvigilante.com/$myIP|jq '.data.latitude'|tr -d '"'|sed 's/.$//')
-	echo 40 | dialog --title "Getting Longitude." --gauge "Please wait ...." 10 60
-	long=$(curl -s https://ipvigilante.com/$myIP|jq '.data.longitude'|tr -d '"'|sed 's/.$//')
-	echo 60 | dialog --title "Getting Forcast Link." --gauge "Please wait ...." 10 60
-	forcastLink=$(curl -s https://api.weather.gov/points/${lat},${long}|jq '.properties.forecast' | tr -d '"')
-	echo 80 | dialog --title "Getting Forcast Information." --gauge "Please wait ...." 10 60
-	currentForcast=$(curl -s $forcastLink | jq '.properties.periods[0].detailedForecast' | tr -d '"')
-	echo 100 | dialog --title "Finalizing." --gauge "Done ...." 10 60
-	dialog_box "$currentForcast"
+	echo 0 | dialog --title "Getting IP Address." --gauge "Please wait ...." 10 60									#Display Loading Bar
+	myIP=$(curl -s ipinfo.io/ip)																					#Get current IP address
+	echo 20 | dialog --title "Getting Latitude." --gauge "Please wait ...." 10 60									#Display Loading Bar
+	lat=$(curl -s https://ipvigilante.com/$myIP|jq '.data.latitude'|tr -d '"'|sed 's/.$//')							#Get my latitude
+	echo 40 | dialog --title "Getting Longitude." --gauge "Please wait ...." 10 60									#Display Loading Bar
+	long=$(curl -s https://ipvigilante.com/$myIP|jq '.data.longitude'|tr -d '"'|sed 's/.$//')						#Get my longitude
+	echo 60 | dialog --title "Getting Forcast Link." --gauge "Please wait ...." 10 60								#Display Loading Bar
+	forcastLink=$(curl -s https://api.weather.gov/points/${lat},${long}|jq '.properties.forecast' | tr -d '"')		#Get weather api link to forcast for my latitude and longitude
+	echo 80 | dialog --title "Getting Forcast Information." --gauge "Please wait ...." 10 60						#Display Loading Bar
+	currentForcast=$(curl -s $forcastLink | jq '.properties.periods[0].detailedForecast' | tr -d '"')				#Store the Detailed forcast to display in message box.
+	echo 100 | dialog --title "Finalizing." --gauge "Done ...." 10 60												#Display Loading Bar
+	dialog_box "$currentForcast"																					#Display Forcast
 }
 
+#Main menu
 while true; do
 	exec 3>&1
 	choice=$(dialog \
@@ -157,11 +149,11 @@ while true; do
 		$DIALOG_OK)
 			case $choice in
 				1)
-					user_menu;;
+					user_menu;;			#User Menu
 				2)
-					system_menu;;
+					system_menu;;		#System Menu
                 3)
-					local_menu;;
+					local_menu;;		#Local Menu
 			esac;;
 		$DIALOG_CANCEL)
 			exec 3>&-
